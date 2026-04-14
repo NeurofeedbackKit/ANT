2d740ee65392fbceb01d1b02e9498984b206e103
payam <payam.sadeghi74@gmail.com>
2026-04-14 07:07:35 +0200

    logging added

--- antares_2.py
+++ antares_2.py
@@ -1,47 +1,14 @@
-import sys
 import yaml
+import logging
 from pathlib import Path
 import time
 import argparse
-import pygame
 from ant import NFRealtime
-
-def show_image_until_space(image_path):
-    pygame.init()
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    image = pygame.image.load(image_path)
-    image = pygame.transform.scale(image, screen.get_size())
-    clock = pygame.time.Clock()
-
-    waiting = True
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    waiting = False
-
-        screen.blit(image, (0, 0))
-        pygame.display.flip()
-
-        clock.tick(60)
-
-    pygame.display.quit()
-    pygame.quit()
-    time.sleep(10) # change this there
 
 if __name__ == "__main__":
 
     with open("config_master.yml", "r") as f:
         config = yaml.safe_load(f)
-
-    ## show instruction img
-    imgs_dir = config.get("imgs_dir", "./")
-    image_path = Path(imgs_dir) / "img_01.jpg"
-    # show_image_until_space(image_path)
 
     ## now connect to stream and record RS EEG
     parser = argparse.ArgumentParser()
@@ -52,8 +19,16 @@ if __name__ == "__main__":
     subject_id = args.subject_id
     visit = int(args.visit)
     subjects_dir = config.get("subjects_dir", "./")
-
+    log_dir = Path(subjects_dir) / subject_id / "logs"
     baseline_duration = config.get("baseline_duration")
+
+    log_file = log_dir / f"v_{visit}.log"
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    )
+    logging.info(f"Baseline recording initiated ...")
     kwargs = {
             "subject_id": subject_id,
             "visit": visit,
@@ -68,6 +43,7 @@ if __name__ == "__main__":
     # Connect to a mock LSL stream (we are using our simulated data)
     fname = "/Users/payamsadeghishabestari/ANT/data/simulated/pericalcarine-lh_10_2-raw.fif"
     nf.connect_to_lsl(mock_lsl=True, fname=fname)
+    logging.info(f"Connected to LSL stream ...")
     time.sleep(4)
     nf.record_baseline(baseline_duration=baseline_duration)
-
+    logging.info(f"Baseline recording finished ...")
