b7d4aad572e269983076abee4effafbbbc1535ea
payam <payam.sadeghi74@gmail.com>
2026-05-11 12:08:03 +0200

    ASR and maxwell added

--- src/ant/tools/__init__.py
+++ src/ant/tools/__init__.py
@@ -1,5 +1,7 @@
 from .tools import *
 from .tools import _compute_inv_operator
-from .simulation import simulate_eeg_raw
+from .simulation import simulate_raw, simulate_eeg_raw
 from .orica import ORICA
 from .gedai import GEDAIDenoiser
+from .asr import ASRDenoiser
+from .maxwell import RTMaxwellFilter
